9cb78c4955f84ce680042f97501f22739b49cc8d
Premky <42573609+Premky@users.noreply.github.com>
2024-09-19 18:00:26 +0545

    Update db.js

--- Server/Utils/db.js
+++ Server/Utils/db.js
@@ -8,13 +8,20 @@ import mysql from 'mysql'
 //     database:'kppo_program'
 // })
 
-const con = mysql.createConnection({
-    host:'localhost',
-    user:'root',    
-    password:'',
-    database:'karagar'
-})
+// const con = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',    
+//     password:'',
+//     database:'karagar'
+// })
 
+const con = mysql.createConnection({
+    host:'prem-prem.d.aivencloud.com',
+    port:'22706',
+    user:'karagar_sankhuwasabha',    
+    password:'AVNS_enpVvs-a73u5I3Trd4M',
+    database:'Karagar'
+})
 con.connect(function(err){
     if(err){
         console.log("Connection Error")
