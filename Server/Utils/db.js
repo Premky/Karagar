9e6aa3afe01d779d45967ef4baeb88d2744ed587
Prem Kumar Yadav <www.prem.mass@gmail.com>
2024-09-20 09:15:37 +0545

    new update on b&f end

--- Server/Utils/db.js
+++ Server/Utils/db.js
@@ -1,5 +1,6 @@
-import mysql from 'mysql'
-// import mysql from 'mysql2/promise';
+// import mysql from 'mysql'
+
+import mysql from 'mysql2';
 
 // const con = mysql.createConnection({
 //     host:'localhost',
@@ -8,15 +9,27 @@ import mysql from 'mysql'
 //     database:'kppo_program'
 // })
 
+// const con = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',    
+//     password:'',
+//     database:'karagar'
+// })
+
+//This uses mysql2
 const con = mysql.createConnection({
-    host:'localhost',
-    user:'root',    
-    password:'',
-    database:'karagar'
+    host:'prem-prem.d.aivencloud.com',
+    port:'22706',
+    // auth_type:'config',
+    user:'karagar_sankhuwasabha',    
+    password:'AVNS_enpVvs-a73u5I3Trd4M',
+    // extension:'mysqli',
+    database:'Karagar'
 })
 
 con.connect(function(err){
     if(err){
+        console.log(err)
         console.log("Connection Error")
     }
     else{
